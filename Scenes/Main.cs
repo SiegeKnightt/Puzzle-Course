9932068c27afe17361f1d5cc689cdbf840dfcba3
Reeves Farrell <reevesfarrell13@gmail.com>
2025-02-26 19:27:37 -0500

    Add in cursor grid functionality

--- Scenes/Main.cs
+++ Scenes/Main.cs
@@ -1,16 +1,33 @@
 using Godot;
 using System;
 
-public partial class Main : Node2D
-{
+namespace Game;
+
+public partial class Main : Node2D {
+
+	private Sprite2D sprite;
+
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		GD.Print("Hello world!");
+	public override void _Ready() {
+
+		sprite = GetNode<Sprite2D>("Cursor");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	public override void _Process(double delta) {
+
+		var mousePosition = GetGlobalMousePosition();
+
+		// Divides the x and y component by 64
+		var gridPosition = mousePosition / 64;
+
+		// Floor rounds the component down
+		gridPosition = gridPosition.Floor();
+
+		// Multiply position by 64 to ensure 
+		// pixel position is aligned with grid
+		sprite.GlobalPosition = gridPosition * 64;
+
+		GD.Print(gridPosition);
 	}
 }
