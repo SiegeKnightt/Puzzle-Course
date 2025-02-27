e153a17f9f5178398713b05213c81b7e2161dd89
Reeves Farrell <reevesfarrell13@gmail.com>
2025-02-26 19:50:48 -0500

    Add ability to place a building using left click

--- Scenes/Main.cs
+++ Scenes/Main.cs
@@ -7,14 +7,39 @@ public partial class Main : Node2D {
 
 	private Sprite2D sprite;
 
+	// PackedScene containes the data required
+	// to instantiate a scene
+	private PackedScene buildingScene;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
+
+		buildingScene = GD.Load<PackedScene>("res://scenes/building/Building.tscn");
 
 		sprite = GetNode<Sprite2D>("Cursor");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
+    public override void _UnhandledInput(InputEvent @event) {
+
+		if (@event.IsActionPressed("left_click")) {
+
+			PlaceBuildingAtMousePosition();
+		}
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) {
+
+		var gridPosition = GetMouseGridCellPosition();
+
+		// Multiply position by 64 to ensure 
+		// pixel position is aligned with grid
+		sprite.GlobalPosition = gridPosition * 64;
+
+		GD.Print(gridPosition);
+	}
+
+	private Vector2 GetMouseGridCellPosition() {
 
 		var mousePosition = GetGlobalMousePosition();
 
@@ -24,10 +49,18 @@ public partial class Main : Node2D {
 		// Floor rounds the component down
 		gridPosition = gridPosition.Floor();
 
-		// Multiply position by 64 to ensure 
-		// pixel position is aligned with grid
-		sprite.GlobalPosition = gridPosition * 64;
+		return gridPosition;
+	}
 
-		GD.Print(gridPosition);
+	private void PlaceBuildingAtMousePosition() {
+
+		var building = buildingScene.Instantiate<Node2D>();
+
+		// Adds a child of the class into the scene tree
+		AddChild(building);
+
+		var gridPosition = GetMouseGridCellPosition();
+
+		building.GlobalPosition = gridPosition * 64;
 	}
 }
